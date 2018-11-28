768d251d6d567969995b62ac2cee596ed484db36
root <root@vultr.guest>
2018-11-28 18:16:52 +0800

    php实现栈，中缀表达式转后缀表达式以及后缀表达式的计算，待详细测试

--- Stack.php
+++ Stack.php
@@ -1,6 +1,6 @@
 <?php
 class Stack{
-    const MAXSIZE=10;//设置栈的大小
+    const MAXSIZE=40;//设置栈的大小
     public $stack=[];//栈
     public $top=-1;//栈顶指针
     public function __construct(){
@@ -33,20 +33,14 @@ class Stack{
 }
 class ReversePolish{
     public $num;
-    public $res;
     
     private $sign=['+','-','*','/'];
     public function __construct(){
         $this->num=new Stack;
     }
     
-    public function calculate($str){
-    	$arr=explode(' ',$str);
-    	$count=count($arr);
+    public function calculate($arr){
     	foreach($arr as $v){
-    		if(!in_array($v, $this->sign) && !is_numeric($v)){
-    			return '字符串中有非法字符';
-    		}
     		if(!in_array($v, $this->sign)){
     			$this->num->push($v);
     		}else{
@@ -73,5 +67,85 @@ class ReversePolish{
     }
 }
 
+class OrdinaryToReversePolish{
+    public $num;
+    public $res;
+    public $tmparr;
+    public $symbol=['+','-','*','/','(',')'];
+    public function __construct(){
+        $this->num=new Stack;
+        $this->res=new Stack;
+        $this->tmparr=new Stack;
+    }
+
+	public function stringchangearray($str){
+        $strarr=str_split($str);
+        foreach($strarr as $v){
+            if(!is_numeric($v) && !in_array($v,$this->symbol)){
+                return false;
+            }
+        }
+        $numpreg='/(\d)*/';
+        preg_match_all($numpreg,$str,$arr);
+        $arr=$arr[0];
+        foreach($strarr as $val){
+            if(!is_numeric($val)){
+                foreach($arr as $key=>$value){
+                    if($value == ""){
+                        $arr[$key]=$val; 
+                        break;
+                    }
+                }
+            }
+        }
+        array_pop($arr);
+        return $this->conversion($arr);
+    }
+    public function conversion($arr){
+        foreach($arr as $v){
+            if(is_numeric($v)){
+                $this->num->push($v);
+            }else{
+                 switch ($v){
+                     case '-':
+                     case '+':
+                     	while ($tmp !='(' && $this->res->top!=-1 ){
+                     		$tmp=$this->res->pop();
+                     		if($tmp !='('){
+                     			$this->num->push($tmp);
+                     		}
+                     	}
+                     	$this->res->push($v);
+                        break;
+                     case '*':
+                     case '/':
+                     	$tmp=$this->res->pop();
+                     	while($tmp=='*' || $tmp=='/'){
+                     		$this->num->push($tmp);
+                     		$tmp=$this->res->pop();
+                     	}
+                     	$this->res->push($tmp);
+                     	$this->res->push($v);
+                     	break;
+                     case '(':
+                     	$this->res->push($v);
+                     	break;
+                     case ')':
+                     	while($tmp == '('){
+                     		$tmp=$this->res->pop();
+                     		$this->num->push($tmp);
+                     	}
+                     	break;                     	                         
+                 }
+            }
+        }
+        while($this->res->top!=-1){
+            	$this->num->push($this->res->pop());
+        }
+        return $this->num->stack;
+    }
+}
+$test=new OrdinaryToReversePolish;
+$arr=$test->stringchangearray('1+2-3*4/5');
 $reversepolish=new ReversePolish;
-$reversepolish->calculate('1 2 + 5 * 3 /');
+$reversepolish->calculate($arr);
